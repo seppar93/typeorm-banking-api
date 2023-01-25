a585f68ef7b6b05e317b2c89431e929352902232
Sepehr Parirokh <sepehrparirokh@gmail.com>
2023-01-25 18:58:39 -0500

    setup local connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello world");
+// create connection is deprecated
+// import {createConnection} from 'typeorm'
+import { DataSource } from "typeorm";
+
+const main = async () => {
+  try {
+    const AppDataSource = new DataSource({
+      type: "postgres",
+      host: "localhost",
+      port: 3306,
+      username: "test",
+      password: "test",
+      database: "typeorm",
+    });
+    console.log("Connected to Postgres");
+  } catch (error) {
+    console.log(error);
+    throw new Error('Unable to connect to database')
+  }
+};
+
+main();
